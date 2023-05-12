a28478395518ea93a3d1fc19a2851227fac21915
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-05-12 11:50:03 -0300

    Finish dashboard

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const btnToggleTheme = document.querySelector("#theme-toggle");
+const sidebar = document.querySelector("#sidebar")
+
+
 function toggleTheme () {
     const theme = document.documentElement;
     
@@ -5,7 +9,21 @@ function toggleTheme () {
     theme.className = "dark" : 
     theme.className = "light";
     
+    btnToggleTheme.classList.toggle("flip")
+    
     console.log(theme.className)
 }
 
+btnToggleTheme.addEventListener("click", toggleTheme)
 toggleTheme();
+
+
+function openCloseSidebar() {
+    const spans = [...document.querySelectorAll(".sidebar span")];
+    spans.forEach(span => {
+        span.classList.toggle("hidden")
+    })
+    console.log(spans)
+}
+
+sidebar.addEventListener("click", openCloseSidebar)
